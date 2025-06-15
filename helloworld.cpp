318c6059a169777f8154566795f0a1379d012128
jiutan <zzzyyyxxx1220@gmail.com>
2025-06-16 02:09:25 +0800

    增加了打印helloworld的程序

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+/**
+ * * @file helloworld.cpp
+ *   @brief 这是一个学习程序：打印hello world
+ */
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+} 
