2eca5f220e882528451ed345c31a3dac879fe5c7
jiutan <zzzyyyxxx1220@gmail.com>
2025-06-16 22:30:47 +0800

    增加了程序选择的方法 if 与 elif

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,7 +4,15 @@ using namespace std;
  * * @file helloworld.cpp
  *   @brief 这是一个学习程序：打印hello world
  */
+
 int main() {
+#if 0
     cout << "Hello, World!" << endl;
     return 0;
+#elif 1
+    cout << "我爱世界!" << endl;
+    return 0;
+#endif
 } 
+
+
