06237e4b1b46128589f1724d555f8e5b586d6347
zyx-linux <zzzyyyxxx1220@gmail.com>
2025-06-29 16:03:24 +0800

    新增习题：3.3.2

---
+++ project/flowpass_Structure/C++Primer/3.3/3.3.2.cpp
@@ -0,0 +1,21 @@
+#include<vector>
+#include<string>
+using std::vector;
+using std::string;
+
+int main(){
+
+#if 0       // 3.3.1
+    vector<vector<int>> ivac;
+
+    // vector<string> svec = ivac;          // 错误
+
+    vector<string> svec(10,"null");
+
+#else 
+
+
+
+#endif
+
+}
